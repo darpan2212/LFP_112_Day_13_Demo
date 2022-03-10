1ff0182bbd846e7f82fd5ca63594d7539dcd2d7d
Darpan Madlani <Darpan@proclink.com>
2022-03-10 21:10:10 +0530

    Generic method

--- src/com/bridgelabz/PrintArray.java
+++ src/com/bridgelabz/PrintArray.java
@@ -2,34 +2,29 @@ package com.bridgelabz;
 
 public class PrintArray {
 
-	public static void toPrint(int[] arr) {
-		for (int i = 0; i < arr.length; i++) {
-			System.out.print(arr[i] + "  ");
+	public static <E> E toPrint(E[] arr) {
+		for (E e : arr) {
+			System.out.print(e+"  ");
 		}
 		System.out.print("\n");
+		return arr[0];
 	}
 
-	public static void toPrint(double[] arr) {
-		for (int i = 0; i < arr.length; i++) {
-			System.out.print(arr[i] + "  ");
-		}
-		System.out.print("\n");
-	}
-
-	public static void toPrint(String[] arr) {
-		for (int i = 0; i < arr.length; i++) {
-			System.out.print(arr[i] + "  ");
-		}
-		System.out.print("\n");
-	}
+	/*
+	 * public static void toPrint(double[] arr) { for (int i = 0; i < arr.length;
+	 * i++) { System.out.print(arr[i] + "  "); } System.out.print("\n"); }
+	 * 
+	 * public static void toPrint(String[] arr) { for (int i = 0; i < arr.length;
+	 * i++) { System.out.print(arr[i] + "  "); } System.out.print("\n"); }
+	 */
 
 	public static void main(String[] args) {
 
-		int[] intArr = new int[] { 1, 2, 3, 6, 8, 9, 11,
-				32 };
+		Integer[] intArr = new Integer[] { 1, 2, 3, 6, 8, 9,
+				11, 32 };
 		toPrint(intArr);
 
-		double[] doubleArr = new double[] { 1.1, 2.4, 3.6,
+		Double[] doubleArr = new Double[] { 1.1, 2.4, 3.6,
 				6.1, 8.2, 9.3, 11.3, 32.7 };
 		toPrint(doubleArr);
 
