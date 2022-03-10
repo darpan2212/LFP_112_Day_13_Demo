b08f6f0cc723d871f6f973138a8e6fa271c8e0a8
Darpan Madlani <Darpan@proclink.com>
2022-03-10 21:20:23 +0530

    generic class solution

--- src/com/bridgelabz/PrintArray.java
+++ src/com/bridgelabz/PrintArray.java
@@ -1,13 +1,12 @@
 package com.bridgelabz;
 
-public class PrintArray {
+public class PrintArray<T> {
 
-	public static <E> E toPrint(E[] arr) {
-		for (E e : arr) {
-			System.out.print(e+"  ");
+	public void toPrint(T[] arr) {
+		for (T t : arr) {
+			System.out.print(t + "  ");
 		}
 		System.out.print("\n");
-		return arr[0];
 	}
 
 	/*
@@ -22,15 +21,18 @@ public class PrintArray {
 
 		Integer[] intArr = new Integer[] { 1, 2, 3, 6, 8, 9,
 				11, 32 };
-		toPrint(intArr);
+		PrintArray<Integer> intPrintArr = new PrintArray<Integer>();
+		intPrintArr.toPrint(intArr);
 
 		Double[] doubleArr = new Double[] { 1.1, 2.4, 3.6,
 				6.1, 8.2, 9.3, 11.3, 32.7 };
-		toPrint(doubleArr);
+		PrintArray<Double> doublePrintArr = new PrintArray<>();
+		doublePrintArr.toPrint(doubleArr);
 
 		String[] stringArr = new String[] { "hello",
 				"world", "how are you?", "Hey!!" };
-		toPrint(stringArr);
+		PrintArray<String> strPrintArr = new PrintArray<>();
+		strPrintArr.toPrint(stringArr);
 	}
 
 }
